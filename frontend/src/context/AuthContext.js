84d578ad6af1bce7ecd2ed65fe91d6931c7d6bd5
mannah-maker <turakhonzodasurush@gmail.com>
2026-02-04 17:04:47 +0300

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -10,7 +10,6 @@ export const useAuth = () => {
   }
   return context;
 };
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
